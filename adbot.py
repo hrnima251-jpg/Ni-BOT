4605fc93344c3281103e0df9c95ed53c112b2d46
hrnima251-jpg <hrnima251@gmail.com>
2026-09-22 19:42:37 +0330

    Update adbot.py

--- adbot.py
+++ adbot.py
@@ -869,7 +869,7 @@ class AdvancedBot(BaseBot):
             "creepycute": "emote-creepycute",
             "frustrated": "emote-frustrated",
             "pose10": "emote-pose10",
-            "relaxedsit": "sit-relaxed",
+            "pepose": "sit-relaxed",
             "stargazing": "emote-stargaze",
             "slap": "emote-slap",
             "boxer": "emote-boxer",
